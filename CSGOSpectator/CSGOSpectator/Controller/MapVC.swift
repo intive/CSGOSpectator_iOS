b5f9aaecd08aaba1248e7b2183b8d3de5e3aaca5
Adam Wienconek <adwienc@gmail.com>
2018-03-25 11:00:39 +0200

    Create MapViewController

---
+++ CSGOSpectator/CSGOSpectator/Controller/MapVC.swift
@@ -0,0 +1,21 @@
+//
+//  MapVC.swift
+//  CSGOSpectator
+//
+//  Created by Adam Wienconek on 25.03.2018.
+//  Copyright © 2018 intive. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+    
+    @IBOutlet weak var mapImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
