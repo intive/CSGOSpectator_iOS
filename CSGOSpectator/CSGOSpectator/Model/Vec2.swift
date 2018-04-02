f7b868d1c7ef1d78aa2781fa6875b699252f088b
Adam Wienconek <wiencheck@gmail.com>
2018-04-02 17:02:20 +0200

    Add Vec2 struct

---
+++ CSGOSpectator/CSGOSpectator/Model/Vec2.swift
@@ -0,0 +1,19 @@
+//
+//  Vec2.swift
+//  CSGOSpectator
+//
+//  Created by Adam Wienconek on 02.04.2018.
+//  Copyright © 2018 intive. All rights reserved.
+//
+
+import UIKit
+
+struct Vec2 {
+    var x: CGFloat
+    var y: CGFloat
+    
+    init(x: CGFloat, y: CGFloat) {
+        self.x = x
+        self.y = y
+    }
+}
