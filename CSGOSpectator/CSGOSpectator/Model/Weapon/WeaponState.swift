396a6b33277534605cbdb895187d8ddf2375d7b6
Adam Wienconek <wiencheck@gmail.com>
2018-04-13 18:05:44 +0200

    Add new enums for Weapon type, name, state and GrenadeType

---
+++ CSGOSpectator/CSGOSpectator/Model/Weapon/WeaponState.swift
@@ -0,0 +1,14 @@
+//
+//  WeaponState.swift
+//  CSGOSpectator
+//
+//  Created by Adam Wienconek on 13.04.2018.
+//  Copyright © 2018 intive. All rights reserved.
+//
+
+import Foundation
+
+enum WeaponState: String, Decodable {
+    case holstered
+    case active
+}
