882741b8924b339b0d0387f943501290870812c6
Magdalena Jedraszak <magdalena.jedraszak@gmail.com>
2018-04-17 23:29:31 +0200

    test testow

--- CSGOSpectator/CSGOSpectatorUITests/CSGOSpectatorUITests.swift
+++ CSGOSpectator/CSGOSpectatorUITests/CSGOSpectatorUITests.swift
@@ -33,7 +33,7 @@ class CSSPPFIP25: XCTestCase {
     }
     func testKillsLabel() {
          XCTContext.runActivity(named: "Checking if Kills Column Label is displayed") { _ in
-            XCTAssertTrue(app.staticTexts["K"].exists)}
+            XCTAssertTrue(app.staticTexts["Kikuhjsfjsfg"].exists)}
     }
     func testMVPLabel() {
         XCTContext.runActivity(named: "Checking if MVP Column Label is displayed") { _ in
