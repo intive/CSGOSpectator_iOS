73884d8b89e1669bbd8570633bf5e48b2388d0f0
Adam Wienconek <adamwienconek@gmail.com>
2018-05-22 11:35:01 +0200

    Customize SettingsVC to match current design (#19)
    * Customize SettingsVC to match current design
    * Correct email validation

--- CSGOSpectator/CSGOSpectator/Controller/SettingsViewController.swift
+++ CSGOSpectator/CSGOSpectator/Controller/SettingsViewController.swift
@@ -9,13 +9,64 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var confirmButton: UIButton!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var underscoreView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textField.delegate = self
+        confirmButton.setImage(#imageLiteral(resourceName: "send_active"), for: .normal)
+        confirmButton.setImage(#imageLiteral(resourceName: "send_not"), for: .disabled)
+        textField.tintColor = UIColor.lightGreen
+        guard let text = textField.text else { return }
+        confirmButton.isEnabled = !text.isEmpty
+        textField.addTarget(self, action: #selector(updateButton(textField:)), for: .editingChanged)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UIApplication.shared.statusBarStyle = .lightContent
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        UIApplication.shared.statusBarStyle = .default
+    }
+    
+    @IBAction func confirmPressed() {
+        print("Confirm pressed")
+    }
+    
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+// MARK: Text Field methods
+extension SettingsViewController: UITextFieldDelegate {
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        emailLabel.textColor = UIColor.lightGreen
+        underscoreView.backgroundColor = UIColor.lightGreen
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        emailLabel.textColor = UIColor.gray
+        underscoreView.backgroundColor = UIColor.gray
+        return true
+    }
+    
+    @objc func updateButton(textField: UITextField) {
+        guard let text = textField.text else { return }
+        confirmButton.isEnabled = text.isEmail
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
     
 }
---
+++ CSGOSpectator/CSGOSpectator/Extensions/String.swift
@@ -0,0 +1,19 @@
+//
+//  String.swift
+//  CSGOSpectator
+//
+//  Created by Adam Wienconek on 19.05.2018.
+//  Copyright © 2018 intive. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var isEmail: Bool {
+        guard !self.lowercased().hasPrefix("mailto:") else { return false }
+        guard let emailDetector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue) else { return false }
+        let matches = emailDetector.matches(in: self, options: NSRegularExpression.MatchingOptions.anchored, range: NSRange(location: 0, length: self.count))
+        guard matches.count == 1 else { return false }
+        return matches[0].url?.scheme == "mailto"
+    }
+}
